a2c4d7c219ce6e4196670d026864443d9074c212
Anna <jukanna2006@gmail.com>
2025-03-06 13:17:55 +0300

    cat and grep

---
+++ grep/s21_grep.h
@@ -0,0 +1,12 @@
+typedef struct {
+  int E_pattern;
+  int I_ignore;
+  int V_invert;
+  int C_count;
+  int L_file_names;
+  int N_num_lines;
+  //   int H_without_names;
+  //   int S_no_errors;
+  //   int F_file_as_pettern;
+  //   int O_only_parts;
+} Flags;
